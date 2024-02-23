b1512706a7317725765b0ee664a159fed543d118
cameronn88 <158166303+cameronn88@users.noreply.github.com>
2024-02-23 15:40:45 +0000

    Add files via upload

---
+++ end.js
@@ -0,0 +1,12 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+
